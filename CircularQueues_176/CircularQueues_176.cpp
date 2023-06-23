d02a8a7224d77b1fb671876c2a5f82ffae4f8d05
riskalaluna <riskalaluna@gmail.com>
2023-06-23 10:59:42 +0700

    jika FRONT_position <= REAR_position

--- CircularQueues_176/CircularQueues_176.cpp
+++ CircularQueues_176/CircularQueues_176.cpp
@@ -72,6 +72,12 @@ public:
 
 		cout << "\nElements in tje queue are...\n";
 
-		//
+		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+		}
 	}
 };
