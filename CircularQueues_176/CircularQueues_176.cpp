912cdfdbc442b8d559c0bad381ac69a8fb5250d3
riskalaluna <riskalaluna@gmail.com>
2023-06-23 11:15:08 +0700

    24

--- CircularQueues_176/CircularQueues_176.cpp
+++ CircularQueues_176/CircularQueues_176.cpp
@@ -108,6 +108,11 @@ int main() {
 			cout << "Menu" << endl;
 			cout << "1. Implement insert operation" << endl;
 			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
 		}
 	}
 }
