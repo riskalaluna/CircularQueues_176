e52f4550e718516c3dc41d4bffc54113f766b52c
riskalaluna <riskalaluna@gmail.com>
2023-06-23 10:56:35 +0700

    cek apakah antrian kosong

--- CircularQueues_176/CircularQueues_176.cpp
+++ CircularQueues_176/CircularQueues_176.cpp
@@ -64,6 +64,10 @@ public:
 		int FRONT_position = FRONT;
 		int REAR_position = REAR;
 
-
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
 	}
 };
