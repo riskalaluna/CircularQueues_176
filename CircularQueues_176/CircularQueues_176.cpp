725744e109fd824fe970bfb7000e91bec3573d94
riskalaluna <riskalaluna@gmail.com>
2023-06-23 09:30:00 +0700

    cek apakah antrian kosong

--- CircularQueues_176/CircularQueues_176.cpp
+++ CircularQueues_176/CircularQueues_176.cpp
@@ -20,6 +20,13 @@ public:
 		//cek apakah antrian penuh
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
+			return;
+		}
+
+		//cek apakah antiran kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
 		}
 	}
 };
